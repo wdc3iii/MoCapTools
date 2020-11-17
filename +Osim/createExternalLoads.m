a549e337f80e6f7ab9803ba7bb7cef0c668382ee
Will Compton <wcompton7@gatech.edu>
2020-11-16 21:27:47 -0500

    create external loads prosthesis change

--- +Osim/createExternalLoads.m
+++ +Osim/createExternalLoads.m
@@ -1,17 +1,17 @@
-function extLoadsFileName = createExternalLoads(devices, sides, extLoadsFileName)
+function extLoadsFileName = createExternalLoads(devices, sides, extLoadsFileName, tf_type)
 % createExternalLoads will generate an external loads xml template file for
 % OpenSim that has information about which force plates apply forces to
-% which feet. 
+% which feet.
 % extLoadsFileName = createExternalLoads(devices, sides, extLoadsFileName);
-% devices should be a cell array of each of the force plate names. 
+% devices should be a cell array of each of the force plate names.
 % sides should be a character vector containing 'r' and 'l' indicating
 % whether the device corresponding to that index applies a force to the
-% left or right foot. 
+% left or right foot.
 % extLoadsFileName is an optional character vector indicating the filename
 % that the xml file should be written to. If if it is not supplied, the
 % external loads file will be written to a random file in the temporary
-% folder. In either case, a path to the xml file will be returned. 
-% 
+% folder. In either case, a path to the xml file will be returned.
+%
 % Ex. createExternalLoads({'Left_Force', 'Right_Force'}, 'lr', 'ExternalLoads.xml')
 
 %% create new external loads file
@@ -29,7 +29,7 @@ extLoads = addChild(openSimDoc, 'ExternalLoads');
 extLoads.setAttribute('name', 'externalloads');
 objs = addChild(extLoads, 'objects');
 for i = 1:length(devices) % add each device as an external force
-    objs.appendChild(createExternalForce(devices{i}, sides(i)));
+    objs.appendChild(createExternalForce(devices{i}, sides(i), tf_type));
 end
 addChild(extLoads, 'groups');
 addChild(extLoads, 'datafile', 'Unassigned');
@@ -39,8 +39,8 @@ addChild(extLoads, 'lowpass_cutoff_frequency_for_load_kinematics', '-1');
 fprintf('Writing external loads xml file to %s.\n', extLoadsFileName);
 xmlwrite(extLoadsFileName, dom);
 %% helper functions
-    % addChild will add a child of tag name childTagName to element, and
-    % set its text content to textContent, if it is supplied
+% addChild will add a child of tag name childTagName to element, and
+% set its text content to textContent, if it is supplied
     function newElem = addChild(element, childTagName, textContent)
         newElem = dom.createElement(childTagName);
         element.appendChild(newElem);
@@ -49,12 +49,18 @@ xmlwrite(extLoadsFileName, dom);
         end
     end
 
-    % createExternalForce creates an element for an external force with the
-    % name given in device, that acts on the side given by side
-    function extForce = createExternalForce(device, side)
+% createExternalForce creates an element for an external force with the
+% name given in device, that acts on the side given by side
+    function extForce = createExternalForce(device, side, tf_type)
         extForce = dom.createElement('ExternalForce');
         extForce.setAttribute('name', [device '_extForce']);
-        addChild(extForce, 'applied_to_body', ['calcn_' side]);
+        if ~strcmp(side, tf_type(2))
+            addChild(extForce, 'applied_to_body', ['calcn_' side]);
+        elseif strcmpi(tf_type(1), 'e')
+            addChild(extForce, 'applied_to_body', 'ARJoint');
+        elseif strcmpi(tf_type(1), 'p') || strcmpi(tf_type(1), 'o')
+            addChild(extForce, 'applied_to_body', 'Foot');
+        end
         addChild(extForce, 'force_expressed_in_body', 'ground');
         addChild(extForce, 'point_expressed_in_body', 'ground');
         addChild(extForce, 'force_identifier', [device '_v']);
